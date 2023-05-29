bb9f2b3386ddcbcd13a907a55651a5454b97c48f
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-05-29 11:38:11 +0300

    fix: DB connection: -rm useNewUrlParser

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
