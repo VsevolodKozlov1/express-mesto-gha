2dd38eab937c9d83c246fa7740440a9b83235f5e
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-05-29 12:49:51 +0300

    fix: default path message

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRouter = require('./user');
 const cardRouter = require('./card');
+const { NOT_FOUND_CODE } = require('../utils/errors');
 
 router.use(userRouter);
 router.use(cardRouter);
+router.get('/*', (req, res) => res.status(NOT_FOUND_CODE).send({ message: 'Указан неверный путь' }));
+router.post('/*', (req, res) => res.status(NOT_FOUND_CODE).send({ message: 'Указан неверный путь' }));
+router.put('/*', (req, res) => res.status(NOT_FOUND_CODE).send({ message: 'Указан неверный путь' }));
+router.patch('/*', (req, res) => res.status(NOT_FOUND_CODE).send({ message: 'Указан неверный путь' }));
+router.delete('/*', (req, res) => res.status(NOT_FOUND_CODE).send({ message: 'Указан неверный путь' }));
 
 module.exports = router;
