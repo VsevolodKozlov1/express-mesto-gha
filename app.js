74b24d3a41cb948ea103c592e74d1bda1ff7fda1
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-05-29 20:04:01 +0300

    fix: review 1 nice-to-have req-limiter

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const app = express();
 app.use(helmet());
+app.use(limiter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
