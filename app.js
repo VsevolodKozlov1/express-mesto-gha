8633cbc6f261908eaddb8bcc495af4f7c111210b
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-05-29 11:24:17 +0300

    fix: DB connection parameter

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.use((req, res, next) => {
   req.user = {
