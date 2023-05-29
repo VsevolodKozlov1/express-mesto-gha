476dbbf093f22da9c09f5b02d770615ea7708b89
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-05-29 12:07:54 +0300

    fix: DB connection: backup

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', { useFindAndModify: false });
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
