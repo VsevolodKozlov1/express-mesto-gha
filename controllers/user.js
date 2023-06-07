2daf937d935ed6d01cb2eabac1a5b8bcb33b1ea5
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-07 13:28:51 +0300

    fix: autotest singin responseCode3

--- controllers/user.js
+++ controllers/user.js
@@ -108,7 +108,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: true,
-      }).send(token);
+      }).send({ jwt: token });
     })
     .catch(() => {
       next(new UnauthorizedError('Вход в аккаунт не выполнен'));
