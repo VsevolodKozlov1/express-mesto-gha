c4aedaba5295263c84b9800dfa8525c235a30ab4
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-07 12:19:58 +0300

    fix: autotest creareUser

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/),
       email: Joi.string().required().email(),
       password: Joi.string().required().pattern(/^[a-zA-Z0-9]{8,30}$/),
     }),
